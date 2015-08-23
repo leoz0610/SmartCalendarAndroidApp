fd40a73991d22a44497c00b31fd6161b738f2f55
leoz0610 <leoz@leozs-MacBook-Pro.local>
2015-08-23 14:34:05 -0500

    2:00 PM 08/23/2015

--- app/src/main/java/com/example/smartcalendar/app/CalendarFragment.java
+++ app/src/main/java/com/example/smartcalendar/app/CalendarFragment.java
@@ -1,17 +1,23 @@
 package com.example.smartcalendar.app;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 import com.example.smartcalendar.app.adaptor.CalendarGridAdaptor;
 
+import java.util.ArrayList;
+
 
 public class CalendarFragment extends Fragment {
     private Activity myActivity = null;
+    private ArrayList<Integer> calendarCells;
+    private CalendarGridAdaptor gridAdaptor;
 
     public CalendarFragment() {
         // Required empty public constructor
@@ -32,12 +38,17 @@ public class CalendarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        GridView calendar = (GridView) myActivity.findViewById(R.id.gridCalendar);
-        CalendarGridAdaptor gridAdaptor = new CalendarGridAdaptor(myActivity, R.layout.fragment_calendar);
-        gridAdaptor.add(1);
-        gridAdaptor.add(2);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        GridView calendar = (GridView) rootView.findViewById(R.id.gridCalendar);
+        calendarCells = new ArrayList<Integer>();
+        calendarCells.add(1);
+        calendarCells.add(2);
+        calendarCells.add(3);
+        calendarCells.add(4);
+        calendarCells.add(5);
+        gridAdaptor = new CalendarGridAdaptor(myActivity, R.layout.fragment_calendar, calendarCells);
         calendar.setAdapter(gridAdaptor);
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
     }
 
 }
--- app/src/main/java/com/example/smartcalendar/app/adaptor/CalendarGridAdaptor.java
+++ app/src/main/java/com/example/smartcalendar/app/adaptor/CalendarGridAdaptor.java
@@ -9,17 +9,17 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import com.example.smartcalendar.app.R;
 
+import java.util.List;
+
 /**
  * Created by leoz on 8/20/15.
  */
 public class CalendarGridAdaptor extends ArrayAdapter<Integer> {
     private int resource = -1;
-    private Activity myActivity = null;
 
-    public CalendarGridAdaptor(Context context, int resource) {
-        super(context, resource);
+    public CalendarGridAdaptor(Context context, int resource, List<Integer> objects) {
+        super(context, resource, objects);
         this.resource = resource;
-        this.myActivity = (Activity) context;
     }
 
     @Override
@@ -27,14 +27,14 @@ public class CalendarGridAdaptor extends ArrayAdapter<Integer> {
         View newView = null;
 
         if (convertView == null) {
-            LayoutInflater inflater = (LayoutInflater)myActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            newView = inflater.inflate(resource, parent, true);
+            LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            newView = inflater.inflate(resource, parent, false);
         } else {
             newView = convertView;
         }
 
-        TextView textView = (TextView) myActivity.findViewById(R.id.txtCalendarCell);
-        textView.setText(position);
+        TextView textView = (TextView) newView.findViewById(R.id.txtCalendarCell);
+        textView.setText(getItem(position).toString());
         return newView;
     }
 }
